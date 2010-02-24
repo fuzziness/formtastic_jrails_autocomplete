fdd293b5ce25d5bf7ae6a8cc358af0c35f95fb05
Michele Franzin <michele.franzin@gmail.com>
2010-02-24 18:55:07 +0100

    autocomplete using external plugin/gem

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 if Object.const_defined?("Formtastic")
   require 'formtastic_autocomplete'
-  Formtastic::SemanticFormBuilder.send(:include, ElanDesign::Formtastic::Autocomplete)
+  Formtastic::SemanticFormBuilder.send(:include, Fuzzilabs::Formtastic::Autocomplete)
 end
